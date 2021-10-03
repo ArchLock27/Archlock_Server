3e675bb2e5bfbb5597840d6dd1bdcf6e97c7bb0c
ArchLock27 <89862540+ArchLock27@users.noreply.github.com>
2021-10-03 09:41:58 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,94 +1,73 @@
 //https://www.eclipse.org/paho/clients/js/
-function enviarmsg() {
-  console.log("Mensaje enviado");
-  var texto = document.getElementById('enviarmensaje').value;
-  message = new Paho.MQTT.Message(texto);
-  message.destinationName = "asilva.fie@unach.edu.ec/test1";
-  client.send(message);
-  document.getElementById('enviarmensaje').value = '';
-}
+
 function LED1_On() {
-	console.log("Led Encendido");
- 	document.getElementById('estado').innerHTML='Encendido';
-	document.getElementById('led').src='/static/images/On.png';
-	
-	message = new Paho.MQTT.Message("ON");
-    	message.destinationName = "asilva.fie@unach.edu.ec/test1";
+	console.log("Leyendo Sensores");
+	message = new Paho.MQTT.Message("Leyendo Sensores");
+    	message.destinationName = "mayra.leon@unach.edu.ec/t1";
     	client.send(message);
   
 }
 function LED1_Off(){	
-	console.log("Led Apagado");
-	document.getElementById('estado').innerHTML='Apagado';
-	document.getElementById('led').src='/static/images/Off.png';
+	//alert("led off");
+	console.log("led off");
 	message = new Paho.MQTT.Message("OFF");
-    	message.destinationName = "asilva.fie@unach.edu.ec/test1";
+    	message.destinationName = "mayra.leon@unach.edu.ec/t1";
     	client.send(message);
- 	
 }
 
-function Leer_sensores(){	
-	console.log("Leyendo Sensores");
-	message = new Paho.MQTT.Message("Leyendo Sensores");
-    	message.destinationName = "asilva.fie@unach.edu.ec/test1";
-    	client.send(message);
- 	
-}
+// Create a client instance
+  //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
+
+  // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
   var options = {
    useSSL: false,
-    userName: "asilva.fie@unach.edu.ec",
-    password: "ANDres_soapmac161293#",
+    userName: "mayra.leon@unach.edu.ec",
+    password: "Valeska2016+",
     onSuccess:onConnect,
     onFailure:doFail
   }
+
   // connect the client
   client.connect(options);
    
   // called when the client connects
   function onConnect() {
     // Once a connection has been made, make a subscription and send a message.
-    console.log("Usted esta Conectado");
-    alert("Usted se ha Conectado");
-    client.subscribe("asilva.fie@unach.edu.ec/test");
-    client.subscribe("asilva.fie@unach.edu.ec/test1");
-    message = new Paho.MQTT.Message("Hola Conectado desde la Web");
-    message.destinationName = "asilva.fie@unach.edu.ec/test1";
+    console.log("Conectado...");
+	
+    client.subscribe("mayra.leon@unach.edu.ec/t2");
+    message = new Paho.MQTT.Message("hola desde la web");
+    message.destinationName = "mayra.leon@unach.edu.ec/t1";
     client.send(message);
 	
   }
+
   function doFail(e){
     console.log(e);
 	
   }
+
   // called when the client loses its connection
   function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
       console.log("onConnectionLost:"+responseObject.errorMessage);
     }
   }
+
   // called when a message arrives
   function onMessageArrived(message) {
-	  console.log("Mensaje Recibido: "+message.payloadString);
-	  const separador = ':';
-	  const m1 = 'Led se mantiene Encendido';
-	  const m2 = 'Led se mantiene Apagado';
-	  mensaje = message.payloadString;
-	  const mensajesep = mensaje.split(':');
+    console.log("Mensaje Recibido:"+message.payloadString);
+    separador='Sensores';
+    mensaje=message.payloadString;
+const mensajesep = mensaje.split(' ');
 	  if(mensaje.includes(separador)){
-		  document.getElementById("sensor1").innerHTML='Estado : '+mensajesep[1];
-		  document.getElementById("sensor2").innerHTML='Estado : '+mensajesep[2];
-		  document.getElementById("recibido").innerHTML='Estado Led - '+mensajesep[0];
-		  
-	  } else if (message.payloadString == "ON") {
-		  console.log("Encendido");	  
-	  } else if (message.payloadString == "OFF") {
-		  console.log("Apagado");	 
-	  } else {
-		  document.getElementById("recibido").innerHTML=message.payloadString;
+	  	document.getElementById("sensor1").innerHTML=mensajesep[1];
+	  	document.getElementById("sensor2").innerHTML=mensajesep[2];
 	  }
-
+    document.getElementById("historial").innerHTML=numero[2];
   }
+  
